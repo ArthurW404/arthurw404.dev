4acbc67e12e168a9b03e775e88632e389db9fa37
Arthur W <arthurwang2000@hotmail.com>
2023-07-02 14:24:34 +1000

    Added contact page using template

--- portfolio-ui/app/page.tsx
+++ portfolio-ui/app/page.tsx
@@ -3,19 +3,15 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
+import { navigation_map, socials } from "./lib/constants";
 
-const navigation: Array<{ name: string, href: string }> = [
-    // { name: "Projects", href: "/projects" },
-    // { name: "Contact", href: "/contact" },
-    // { name: "Contact", href: "/" },
-];
 
 export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
             <nav className="my-16 animate-fade-in">
                 <ul className="flex items-center justify-center gap-4">
-                    {navigation.map((item) => (
+                    {navigation_map.map((item) => (
                         <Link
                             key={item.href}
                             href={item.href}
@@ -29,15 +25,15 @@ export default function Home() {
             <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
             <Particles
                 className="absolute inset-0 -z-10 animate-fade-in"
-                quantity={500}
+                quantity={50}
             />
             <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
                 ArthurW404
             </h1>
             <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
             <div className="my-16 animate-fade-in">
-                <h2 className="text-lg text-zinc-500 ">
-                    Hi, I'm Arthur.
+                <h2 className="text-lg text-zinc-500 text-center ">
+                    Hi, I&apos;m Arthur.
                     A software engineering student from the <Link
                         target="_blank"
                         href="https://www.unsw.edu.au/"
@@ -45,9 +41,18 @@ export default function Home() {
                     >
                         University of New South Wales
                     </Link>.
-                    <br/>
-                    
+                    <br />
                 </h2>
+
+                <div className="my-4 flex flow-row justify-center gap-3">
+                    {socials.map(item => (
+                        <Link href={item.href}>
+                            <span className="hover:bg-slate-100 relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+                                {item.icon}
+                            </span>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     );
