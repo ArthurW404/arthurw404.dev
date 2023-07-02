2da9c9fae65dd599234d1b41406d8df9ed7f54b1
Arthur W <arthurwang2000@hotmail.com>
2023-07-02 14:48:55 +1000

    Fixed build

--- portfolio-ui/app/layout.tsx
+++ portfolio-ui/app/layout.tsx
@@ -1,21 +1,21 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Arthur\'s website',
-  description: 'Landing page to Arthur\'s website',
-}
+  title: "Arthur's website",
+  description: "Landing page to Arthur's website",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
