6e24a9496d20823436d787d0ab6dbaaf0687b6c9
Arthur W <arthurwang2000@hotmail.com>
2023-07-16 17:53:47 +1000

    Added more purple, updated style

---
+++ portfolio-ui/app/components/animation/fadeInOutLine.tsx
@@ -0,0 +1,9 @@
+export const FadeInOutLine = ({ goRight = false }) => {
+  if (goRight)
+    return (
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+    );
+  return (
+    <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+  );
+};
